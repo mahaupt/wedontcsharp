6faca32390b4c06ca6264de8552664420de12361
captain13chris <christophermuth@Christophers-iMac.local>
2015-10-07 20:04:03 +0200

    Einfachster Beschleunigungs-Test

--- teams/blau/beschleunigung.m
+++ teams/blau/beschleunigung.m
@@ -1,3 +1,21 @@
 function bes = beschleunigung(spiel, farbe)
     bes = [0, 1];
+    
+    % Teamfarben-Abfrage und Parameter-Zuweisung
+    if strcmp(farbe, 'blau')
+        ich = spiel.blau;
+        gegner = spiel.rot;
+    else
+        ich = spiel.rot
+        gegner = spiel.blau
+    end
+   
+    % Blödsinniger Test xD
+    if spiel.i_t<120
+        bes=[0,1];
+    else
+        bes = [-1,-0.5]; 
+    end
+    
+    
 end
