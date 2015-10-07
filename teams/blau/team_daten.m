0cf58377aecd20004fd17575b057849b40f5cb3d
cbacon93 <subcortexx@gmail.com>
2015-10-07 11:50:13 +0200

    Team Daten angepast

--- teams/blau/team_daten.m
+++ teams/blau/team_daten.m
@@ -1,18 +1,18 @@
 function team = team_daten
 
-team.name = 'Daleks';
+team.name = 'Escalators';
 
-team.mitarbeiter(1).name = 'Dalek Sec';
-team.mitarbeiter(1).aufgabe = 'Angriff';
+team.mitarbeiter(1).name = 'Christopher Muth';
+team.mitarbeiter(1).aufgabe = '';
 
-team.mitarbeiter(2).name = 'Dalek Caan';
-team.mitarbeiter(2).aufgabe = 'Verteidigung';
+team.mitarbeiter(2).name = 'Marcel Haupt';
+team.mitarbeiter(2).aufgabe = '';
 
-team.mitarbeiter(3).name = 'Dalek Jast';
-team.mitarbeiter(3).aufgabe = 'Tanken';
+team.mitarbeiter(3).name = 'Julian Wiggers';
+team.mitarbeiter(3).aufgabe = '';
 
-team.mitarbeiter(4).name = 'Dalek Thay';
-team.mitarbeiter(4).aufgabe = 'Minen';
+team.mitarbeiter(4).name = 'Tobias Kuhlenschmidt';
+team.mitarbeiter(4).aufgabe = '';
 
 team.mitarbeiter(5).name = 'Logo';
 team.mitarbeiter(5).aufgabe = '';
