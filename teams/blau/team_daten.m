18ba5bbbe49771d48dbe4c0669eb9e7bce608766
jwigge <julian.wiggers@gmail.com>
2015-10-07 23:02:58 +0200

    pm

--- teams/blau/team_daten.m
+++ teams/blau/team_daten.m
@@ -8,7 +8,7 @@ team.mitarbeiter(1).aufgabe = 'Master of Desaster';
 team.mitarbeiter(2).name = 'Marcel Haupt';
 team.mitarbeiter(2).aufgabe = '';
 
-team.mitarbeiter(3).name = 'Julian pm Wiggers';
+team.mitarbeiter(3).name = 'Julian Wiggers';
 team.mitarbeiter(3).aufgabe = '';
 
 team.mitarbeiter(4).name = 'Tobias Kuhlenschmidt';
