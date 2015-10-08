717174d281f797d5811a70eed41f12f072730e97
cbacon93 <subcortexx@gmail.com>
2015-10-08 11:41:54 +0200

    Pathfinding System fertiggestellt

--- teams/blau/beschleunigung.m
+++ teams/blau/beschleunigung.m
@@ -1,6 +1,7 @@
 function bes = beschleunigung(spiel, farbe)
     %Konstanten
     constSafeBorder = 0.001;
+    constGridRadius = 0.01;
     
     %statische variablen definieren
     persistent nodeGrid;
@@ -9,31 +10,34 @@ function bes = beschleunigung(spiel, farbe)
     
     %%wird einmal am Anfang ausgeführt
     if (isempty(nodeGrid))
-        setupNodeGrid() 
+        setupNodeGrid()
+        findPath([0.5,0.5], [0.3, 0.3]);
     end
     
     bes = [0, 1];
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     function setupNodeGrid()
-        gridRadius = 0.01;
-        
-        gridSizeX = round(1/(gridRadius*2));
-        gridSizeY = round(1/(gridRadius*2));
+        gridSizeX = round(1/(constGridRadius*2));
+        gridSizeY = round(1/(constGridRadius*2));
         
         %create grid
         for x = 1 : gridSizeX+1
             for y = 1 : gridSizeY+1
-                worldPos = [gridRadius*(x*2-1), gridRadius*(y*2-1)];
+                worldPos = [constGridRadius*2*x, constGridRadius*2*y];
                 gridPos = [x, y];
                 nodeGrid(x,y).worldPos = worldPos;
                 nodeGrid(x,y).gridPos = gridPos;
-                nodeGrid(x,y).isWalkable = isWalkable(worldPos);            
+                nodeGrid(x,y).isWalkable = isWalkable(worldPos);
+                nodeGrid(x,y).hCost = 0;
+                nodeGrid(x,y).fCost = 0;
+                nodeGrid(x,y).gCost = 0;
             end
         end
     end
 
-    %false wenn Wand oder Mine, true wenn frei
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Kollisionscheck
     function erg=isWalkable(pos)
         erg = true;
         secureSpaceballRadius = constSafeBorder + spiel.spaceball_radius;
@@ -51,5 +55,119 @@ function bes = beschleunigung(spiel, farbe)
                 return;
             end
         end
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Pathfinder
+    function waypoints = findPath(startp, endp)
+        pathSuccess = false; % - Pfad gefunden
+        
+        startPos = worldPosToGridPos(startp);
+        endPos = worldPosToGridPos(endp);
+        startNode = nodeGrid(startPos(1),startPos(2));
+        endNode = nodeGrid(endPos(1),endPos(2));
+        
+        openSet = [startNode];
+        closedSet = [];
+        
+        while(numel(openSet) > 0)
+            currentNode = openSet(1);
+            openSet(1) = [];
+            
+            %get node woth lowest fcost (or hcost) and remove it from openlist
+            for i=1:numel(openSet)    
+                if (openSet(i).fCost < currentNode.fCost || openSet(i).fCost == currentNode.fCost && openSet(i).hCost > currentNode.hCost)
+                    currentNode = openSet(i);    
+                end
+            end
+            
+            %if it is target - close - path found!
+            if (equalsNode(currentNode, endNode))
+                pathSuccess = true;
+                break;
+            end
+            
+            %calculate neighbour cost indices
+            neighbours = getNeighbourNodes(currentNode);
+            for i = 1 : numel(neighbours)
+                neighbour = neighbours(i);
+                if (~neighbour.isWalkable)
+                    continue;
+                end
+                
+                %update costs for neighbours
+                movementCostToNeighbour = currentNode.gCost + norm(currentNode.worldPos - neighbour.worldPos);
+                if (movementCostToNeighbour < neighbour.gCost)
+                    if (~ismember(openSet, neighbour))
+                        neighbour.gCost = movementCostToNeighbour;
+                        neighbour.hCost = norm(endNode.worldPos - neighbour.worldPos);
+                        neighbour.fCost = neighbour.gCost + neighbour.hCost;
+                        neighbour.parent = currentNode;
+
+                        %write update
+                        nodeGrid(neighbour.gridX, neighbour.gridY) = neighbour;
+
+                        %add neighbour to openSet
+                        if (~ismember(openSet, neighbour))
+                            append(openSet, neighbour);
+                        end
+                    end
+                end
+            end
+        end
+        
+        %finished pathfinding
+        if (pathSuccess)
+            %retrace path
+            currentNode = endNode;
+            waypoints = [];
+            
+            while (~equalsNode(currentNode,startNode))
+               append(waypoints, currentNode.worldPos);
+               currentNode = currentNode.parent;
+            end
+            
+            %flip waypoints
+            waypoints = simplifyPath(fliplr(waypoints));
+        else
+            waypoints = [];
+        end
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function erg = worldPosToGridPos(pos)
+        erg = [round(pos(1)/constGridRadius/2), round(pos(2)/constGridRadius/2)];
+    end
+
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %check if nodes are equal
+    function erg = equalsNode(a, b)
+        erg = false;
+        if (a.gridPos(1) == b.gridPos(1) && a.gridPos(2) == b.gridPos(2))
+            erg = true;
+        end
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %get node neighbours
+    function erg = getNeighbourNodes(node)
+        i = 1;
+        for x = node.gridPos(1) -1 : node.gridPos(1) +1
+            for y = node.gridPos(2) -1 : node.gridPos(2) + 1
+                if (equalsNode(nodeGrid(x, y), node))
+                    continue;
+                end
+                
+                erg(i) = nodeGrid(x, y);
+                i = i + 1;
+            end
+        end
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % simplify path
+    function erg = simplifyPath(path)
+        erg = path;
     end
 end
