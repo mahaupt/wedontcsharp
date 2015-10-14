82a1080b58b8c2617b5e2ef5f8a4080ff72ce392
tkuhle <tobias.kuhlenschmidt@gmail.com>
2015-10-14 12:58:21 +0200

    Statistik Funktion
    s. ToDo Kommentar

---
+++ statistics.m
@@ -0,0 +1,16 @@
+function [erg] = statistics(n)
+  
+% todo ergebnisse speichern, gewinnbilanz erstellen, einsehmöglichkeit warum man 0
+% punkte bekommen hat (crash mit mine oder gegner)
+
+    for i = 1 : 1 : n
+        spaceballs
+                  
+        result_rot = spiel.rot.punkte
+        result_blau = spiel.blau.punkte
+        
+    end    
+
+   
+end
+
