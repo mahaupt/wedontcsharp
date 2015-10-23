7e2a4f0d11629a7864db03760488dc85a4d22475
captain13chris <ChristopherMuth@Chris-MacBook-Pro.local>
2015-10-23 12:45:46 +0200

    Statistik Namen der Gegner hinzugefügt

--- spaceballs.m
+++ spaceballs.m
@@ -1,12 +1,12 @@
 %% Initialisierung
 
-clear variables
+%clear variables
 close all
 clc
 
 rng shuffle
 r = round(rand(1)*1000000)
-rng(386153);
+rng(r);
 
 
 %% Konstanten definieren
@@ -47,7 +47,7 @@ spiel.spur_anfangswert = 1*spiel.n_t;
 spiel.zeitlupe_anfangswert = 0;
 spiel.ges_checkbox_anfangswert = true;
 spiel.bes_checkbox_anfangswert = true;
-spiel.zeitraffer_checkbox_anfangswert = false;
+spiel.zeitraffer_checkbox_anfangswert = true;
 
 spiel.rot.spur(spiel.n_t, 2) = 0;
 spiel.blau.spur(spiel.n_t, 2) = 0;
