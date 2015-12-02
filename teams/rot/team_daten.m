855474c29f108007bca699aed039ab735d4d14e1
captain13chris <christopher.muth@me.com>
2015-12-02 23:38:24 +0100

    aufgeräumt

--- teams/rot/team_daten.m
+++ teams/rot/team_daten.m
@@ -1,6 +1,5 @@
 function team = team_daten
 
-<<<<<<< HEAD
 team.name = 'Florians KI';
 
 team.mitarbeiter(1).name = 'Gordon Freeman';
@@ -14,21 +13,6 @@ team.mitarbeiter(3).aufgabe = 'Tanken';
 
 team.mitarbeiter(4).name = 'Isaac Kleiner';
 team.mitarbeiter(4).aufgabe = 'Minen';
-=======
-team.name = 'We don''t C#, we MethLab ';
-
-team.mitarbeiter(1).name = 'Christopher Muth';
-team.mitarbeiter(1).aufgabe = 'Master Of Desaster';
-
-team.mitarbeiter(2).name = 'Marcel Haupt';
-team.mitarbeiter(2).aufgabe = 'Master Of Pathfinding';
-
-team.mitarbeiter(3).name = 'Julian Wiggers';
-team.mitarbeiter(3).aufgabe = 'Director Of Attack';
-
-team.mitarbeiter(4).name = 'Tobias Kuhlenschmidt';
-team.mitarbeiter(4).aufgabe = 'Director Of Defence';
->>>>>>> master
 
 team.mitarbeiter(5).name = 'Logo';
 team.mitarbeiter(5).aufgabe = '';
