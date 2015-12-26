3c72257c428eaddd60718b7f0e307cc8c7032d0c
jwigge <julian.wiggers@gmail.com>
2015-12-26 18:19:34 +0100

    Doku

--- spaceballs.m
+++ spaceballs.m
@@ -6,7 +6,7 @@ clc
 
 rng shuffle
 r = round(rand(1)*1000000)
-rng(r);
+rng(476211);
 
 %% Konstanten definieren
 
@@ -16,7 +16,7 @@ spiel.n_t = round (spiel.t_end/spiel.dt);
 spiel.i_t = 0;
 
 spiel.n_mine = 12; % Muss gerade sein! (Default: 12)
-spiel.n_tanke = 9; % Muss ungerade sein! (Default: 9)
+spiel.n_tanke = 3; % Muss ungerade sein! (Default: 9)
 
 spiel.kreis_radius = 0.075;
 spiel.mine_radius = 0.05;
