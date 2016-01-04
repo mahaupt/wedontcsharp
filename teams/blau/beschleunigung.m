8f38f8e4464b82b231eea866d5e61b980ab621b1
jwigge <julian.wiggers@gmail.com>
2016-01-05 00:40:02 +0100

    ; eingefügt. Default Tanken auf 9.

--- teams/blau/beschleunigung.m
+++ teams/blau/beschleunigung.m
@@ -1347,7 +1347,7 @@ function bes = beschleunigung(spiel, farbe)
         for i=1:spiel.n_mine 
             checktime = defMineTime(spiel.mine(i).pos); %Zeitdiff. für alle Ecken berechnen 
             if (savetime_mine < checktime) %Zeit für Ecke 1 überschreibt savetime und wir als neue savetime gespeichert. Die neue Savetime wird nur von größeren Zeitdiffs überschrieben. 
-                savetime_mine = checktime
+                savetime_mine = checktime;
             end
         end
                  
