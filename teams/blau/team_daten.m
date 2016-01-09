eddafc74bdbd88a9836d1cb443d1efa5d90e09de
captain13chris <christopher.muth@me.com>
2016-01-09 15:15:04 +0100

    Teammitglieder Aufgaben aktualisiert

--- teams/blau/team_daten.m
+++ teams/blau/team_daten.m
@@ -3,10 +3,10 @@ function team = team_daten
 team.name = 'We don´t C#, we Methlab';
 
 team.mitarbeiter(1).name = 'Christopher Muth';
-team.mitarbeiter(1).aufgabe = 'Master Of Desaster';
+team.mitarbeiter(1).aufgabe = 'Director Of Refueling';
 
 team.mitarbeiter(2).name = 'Marcel Haupt';
-team.mitarbeiter(2).aufgabe = 'Master Of Pathfinding';
+team.mitarbeiter(2).aufgabe = 'Director Of Pathfinding';
 
 team.mitarbeiter(3).name = 'Julian Wiggers';
 team.mitarbeiter(3).aufgabe = 'Director Of Attack';
